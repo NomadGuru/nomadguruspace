17741a2514f4b6d5115db89a9e4c921827f3c0de
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-07-04 19:37:27 +0200

    Init app folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image'
-  ]
-})
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+});
