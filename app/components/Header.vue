7cc784e1cfc3d4ad1629a932e280ff80f0a6d725
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-07-05 18:09:58 +0200

    Add header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,138 +1,58 @@
 <template>
-  <header class="root sticky-top d-print-none" id="sticky-from-here">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">
-          <img
-            data-bs-theme="light"
-            alt="NOMAD GURU"
-            src="~/assets/img/logo/black-wide.png"
-          /><img
-            data-bs-theme="dark"
-            alt="NOMAD GURU"
-            style="display: none"
-            src="~/assets/img/logo/white-wide.png"
-          />
-        </a>
-        <button
-          class="hamburger hamburger--slider d-lg-none"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#headerNavbar"
-          aria-controls="headerNavbar"
-          aria-expanded="false"
-          aria-label=""
-        >
-          <span class="hamburger-box">
-            <span class="hamburger-inner"></span>
-          </span>
-          <!-- <span class="navbar-toggler-icon"></span> -->
-        </button>
-        <div class="collapse navbar-collapse" id="headerNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="art.html">
-                <span>ART</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="house.html">
-                <span>HOUSE</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="school.html">
-                <span>SCHOOl</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">
-                <span>ПРО НАС</span>
-              </a>
-            </li>
-          </ul>
-          <div class="search">
-            <button
-              class="btn btn-warning"
-              type="button"
-              data-bs-toggle="modal"
-              data-bs-target="#searchModal"
-              nw-hotkey="cmd+k|s|и"
+  <header>
+    <nav class="navbar bg-base-100 shadow-sm">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <span class="icon-search fs-3"></span>
-              <span class="d-xl-none">Поїхали</span>
-            </button>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-
-          <ul class="navbar-nav lang align-items-center">
-            <li class="dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                title="Change language"
-                nw-lang="uk"
-                ><img
-                  src="~/assets/img/lang/uk.svg"
-                  alt="Українська"
-                  style="max-height: 3rem"
-                /><span><span>Українська</span></span></a
-              >
-              <ul class="dropdown-menu dropdown-menu-end">
-                <li>
-                  <a
-                    class="dropdown-item active"
-                    href="ru/index.html"
-                    nw-lang="uk"
-                    ><img
-                      src="~/assets/img/lang/uk.svg"
-                      alt="Українська"
-                      style="max-height: 3rem"
-                    /><span><span>Українська</span></span></a
-                  >
-                </li>
-
-                <li>
-                  <a class="dropdown-item" href="/en/index.html" nw-lang="en"
-                    ><img
-                      src="~/assets/img/lang/en.svg"
-                      alt="English"
-                      style="max-height: 3rem"
-                    /><span><span>English</span></span></a
-                  >
-                </li>
-
-                <li>
-                  <a class="dropdown-item" href="/ru/index.html" nw-lang="ru"
-                    ><img
-                      src="~/assets/img/lang/ru.svg"
-                      alt="Русский"
-                      style="max-height: 3rem"
-                    /><span><span>Русский</span></span></a
-                  >
-                </li>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li><a>Item 1</a></li>
+            <li>
+              <a>Parent</a>
+              <ul class="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
               </ul>
             </li>
+            <li><a>Item 3</a></li>
           </ul>
-
-          <div class="logo">
-            <img
-              data-bs-theme="light"
-              alt="NOMAD GURU"
-              src="~/assets/img/logo/black@2d.png"
-            /><img
-              data-bs-theme="dark"
-              alt="NOMAD GURU"
-              style="display: none"
-              src="~/assets/img/logo/white@2d.png"
-            />
-          </div>
         </div>
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li><a>Item 1</a></li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul class="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </details>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
+      <div class="navbar-end">
+        <a class="btn">Button</a>
       </div>
     </nav>
   </header>
---
+++ app/components/HeaderOld.vue
@@ -0,0 +1,139 @@
+<template>
+  <header class="root sticky-top d-print-none" id="sticky-from-here">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">
+          <img
+            data-bs-theme="light"
+            alt="NOMAD GURU"
+            src="~/assets/img/logo/black-wide.png"
+          /><img
+            data-bs-theme="dark"
+            alt="NOMAD GURU"
+            style="display: none"
+            src="~/assets/img/logo/white-wide.png"
+          />
+        </a>
+        <button
+          class="hamburger hamburger--slider d-lg-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#headerNavbar"
+          aria-controls="headerNavbar"
+          aria-expanded="false"
+          aria-label=""
+        >
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+          <!-- <span class="navbar-toggler-icon"></span> -->
+        </button>
+        <div class="collapse navbar-collapse" id="headerNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="art.html">
+                <span>ART</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="house.html">
+                <span>HOUSE</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="school.html">
+                <span>SCHOOl</span>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">
+                <span>ПРО НАС</span>
+              </a>
+            </li>
+          </ul>
+          <div class="search">
+            <button
+              class="btn btn-warning"
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#searchModal"
+              nw-hotkey="cmd+k|s|и"
+            >
+              <span class="icon-search fs-3"></span>
+              <span class="d-xl-none">Поїхали</span>
+            </button>
+          </div>
+
+          <ul class="navbar-nav lang align-items-center">
+            <li class="dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                title="Change language"
+                nw-lang="uk"
+                ><img
+                  src="~/assets/img/lang/uk.svg"
+                  alt="Українська"
+                  style="max-height: 3rem"
+                /><span><span>Українська</span></span></a
+              >
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                  <a
+                    class="dropdown-item active"
+                    href="ru/index.html"
+                    nw-lang="uk"
+                    ><img
+                      src="~/assets/img/lang/uk.svg"
+                      alt="Українська"
+                      style="max-height: 3rem"
+                    /><span><span>Українська</span></span></a
+                  >
+                </li>
+
+                <li>
+                  <a class="dropdown-item" href="/en/index.html" nw-lang="en"
+                    ><img
+                      src="~/assets/img/lang/en.svg"
+                      alt="English"
+                      style="max-height: 3rem"
+                    /><span><span>English</span></span></a
+                  >
+                </li>
+
+                <li>
+                  <a class="dropdown-item" href="/ru/index.html" nw-lang="ru"
+                    ><img
+                      src="~/assets/img/lang/ru.svg"
+                      alt="Русский"
+                      style="max-height: 3rem"
+                    /><span><span>Русский</span></span></a
+                  >
+                </li>
+              </ul>
+            </li>
+          </ul>
+
+          <div class="logo">
+            <img
+              data-bs-theme="light"
+              alt="NOMAD GURU"
+              src="~/assets/img/logo/black@2d.png"
+            /><img
+              data-bs-theme="dark"
+              alt="NOMAD GURU"
+              style="display: none"
+              src="~/assets/img/logo/white@2d.png"
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
