055fc29a627c812e094afe0199106678c00b50be
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-07-05 18:16:42 +0200

    Add tailwind and daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: false },
   future: {
     compatibilityVersion: 4,
   },
-  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  css: ["~/assets/css/main.css"],
   // css: ["~/assets/css/guru/index.css"],
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
