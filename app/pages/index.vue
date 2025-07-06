a2a1a1b47fd5489aa88ef644aa4b068c9a073902
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-07-06 20:31:33 +0200

    Move images to public

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,20 +1,19 @@
 <template>
   <div
     class="hero min-h-screen"
-    style="
-      background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);
-    "
+    style="background-image: url(/images/house/house.webp)"
   >
     <div class="hero-overlay"></div>
     <div class="hero-content text-neutral-content text-center">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-        <p class="mb-5">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+        <h1 class="mb-5 text-5xl font-bold">Студія Звукозапису</h1>
+        <p class="mb-5 text-base">
+          Наш ДІМ NOMAD GURU. МІСЦЕ, ДЕ МИ ЖИВЕМО І ТВОРИМО, де ми пишемо свої
+          історії. Кожен своєю мовою. Ми різні, але нас поєднує музика, краса та
+          любов до ЖИТТЯ. Ми - NOMAD GURU HOUSE - студія імпровізації та
+          потокової творчості.
         </p>
-        <button class="btn btn-primary">Get Started</button>
+        <button class="btn btn-primary">Дізнатися Більше</button>
       </div>
     </div>
   </div>
